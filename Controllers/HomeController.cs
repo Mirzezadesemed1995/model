b672b064a4683a6cc9e1eb2178a2082fc0b72508
Mirzezadesemed1995 <samedmirzezade@gmail.com>
2023-12-05 21:11:37 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using model_homework.Models.Customers.Entities;
+
+namespace model_homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<About> about=new List<About>() {
+                new About(){Id=1 ,Title="Our Vision",Description="Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra" } ,
+                new About(){Id=2 ,Title="Our Mission",Description="Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis"},
+                new About(){Id=3 ,Title="Our Goal",Description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."}
+            };    
+
+
+            return View(about);
+        }
+    }
+}
